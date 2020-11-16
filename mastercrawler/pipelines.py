d5246ae8b956641fbbb3da6fa8abb2fe92bcc9db
AndreuJove <andreujove@gmail.com>
2020-11-16 19:42:54 +0100

    changed id file

--- mastercrawler/pipelines.py
+++ mastercrawler/pipelines.py
@@ -5,19 +5,20 @@ class MastercrawlerPipeline(object):
     """    
     After an item has been scraped by a spider, it is sent to the Item Pipeline which processes it through several components that are executed sequentially.
     """
-    #Access stats from item pipeline
+    
     def __init__(self, stats):
+        # Access stats from item pipeline
         self.stats = stats
         self.counter = 0
 
-    #Return stats from item pipeline to the crawler.
     @classmethod
     def from_crawler(cls, crawler):
+        # Return stats from item pipeline to the crawler.
         return cls(crawler.stats)
 
-    #Write on a json file. Input: data and path of the file.
     @staticmethod
     def write_json(data, path):
+        # Write on a json file. Input: data and path of the file.
         with open(path, 'w') as f:
             json.dump(data, f)
     
@@ -27,14 +28,21 @@ class MastercrawlerPipeline(object):
         with open(path_file) as f:
             return json.load(f)
 
+    @staticmethod
+    def process_id(tool):
+        if isinstance(tool['idTool'], list):
+            id = tool['idTool'][0]
+        id = tool['idTool'].split("/")[-1]
+        return id
+
     #Check if item has no error, and append on file his correspoding file.
-    def append_item_for_crawling_js(self, item, ):
+    def append_item_for_crawling_js(self, item, final_id):
         item_no_error = {
                             'final_url': item['final_url_tool'], 
                             'id': item['idTool'], 
                             'name' : item['nameTool'],
                             'first_url' : item['first_url_tool'], 
-                            'path_file' : f"entry_{self.counter}_no_js"
+                            'path_file' : f"{final_id}.json"
                         }
         path_final_file = f'{self.args.output_directory}/{self.args.filename_output}.json'
         try:
@@ -54,10 +62,10 @@ class MastercrawlerPipeline(object):
 
     #Recieve item. Parse item. Write item on a json file for posterior access.
     def process_item(self, item, spider):
-        self.counter += 1
         if item['error_name'] == None:
-            item_no_error = self.append_item_for_crawling_js(item)
-            path_tool = f"{self.args.o_directory_htmls_no_js}/{item_no_error['path_file']}.json"
+            final_id = self.process_id(item)
+            item_no_error = self.append_item_for_crawling_js(item, final_id)
+            path_tool = f"{self.args.o_directory_htmls_no_js}/{final_id}.json"
             tool_dict = {
                         'id': item['idTool'], 
                         'html_no_js' : item['html_no_js']

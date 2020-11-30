574ebc50c01a66e11e44aa8362af7be87f8bafc0
AndreuJove <andreujove@gmail.com>
2020-11-30 12:16:02 +0100

    cleaned whitespaces

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-
-""" 
-
-This module starts the process of crawling along X websites and extract his corresponding HTML without JavaScript rendered. 
-Furthermore extracts the statistics from the amount of websites given as input.
-
 """
-
+This module starts the process of crawling along X websites and extract and save
+his corresponding HTML without JavaScript rendered.
+Furthermore extracts the statistics from the amount of websites given as input.
+"""
 import argparse
 import os
 import logging
@@ -13,7 +10,6 @@ import json
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from mastercrawler.spiders.crawlertools import ToolsSpider
-
 
 def load_json(path_file):
     # Load JSON file
@@ -25,13 +21,20 @@ def write_json(data, path):
     with open(path, 'w') as file:
         json.dump(data, file)
 
+def create_dict_manifest(key, value):
+    # Create the final dict key value
+    dict_for_json = {}
+    dict_for_json.setdefault(key, value)
+    return dict_for_json
+
 def main(args):
     # Open the file of list tools unique and save it in a list of dictionaries:
     websites_to_crawl = load_json(args.i_path_file)
 
-    # Create empty manifest file
-    dict_manifest = {}
-    dict_manifest.setdefault("tools_ok", [])
+    # Create empty manifest file.
+    dict_manifest = create_dict_manifest("tools_ok", [])
+
+    # Create the JSON file of the dict.
     write_json(dict_manifest, f'{args.output_directory}/{args.filename_output}.json')
 
     # Create the logger:
@@ -43,21 +46,23 @@ def main(args):
                         filemode='w'
                         )
 
-    # Instance of CrawlerProcess and call the method to access the settings of the crawler in settings.py:
+    # Instance of CrawlerProcess and get the settings of from settings.py:
     process = CrawlerProcess(get_project_settings())
 
     # Method to crawl the spider in crawlertools.py and pass as **kwargs the following variables:
     process.crawl(ToolsSpider,
                     args=args,
-                    list_unique_url=websites_to_crawl['websites_to_crawl'][:10])
+                    list_unique_url=websites_to_crawl['websites_to_crawl'])
 
     # Start the process of crawling.
     process.start()
 
+    # Collect all the HTMLs saved
     list_files = os.listdir(args.o_directory_htmls_no_js)
 
     logging.info(f"Websites INPUT: {len(websites_to_crawl['websites_to_crawl'])}")
     logging.info(f"HTMLs saved: {len(list_files)}")
+
 
 if __name__ == "__main__":
 
@@ -80,7 +85,8 @@ if __name__ == "__main__":
                         type=str,
                         metavar="",
                         default="output_data",
-                        help="Name of the directory for the outputs file")
+                        help="Name of the directory for the outputs file"
+                        )
 
     # Output manifest name file:
     parser.add_argument(
@@ -88,7 +94,7 @@ if __name__ == "__main__":
                         type=str,
                         metavar="",
                         default="manifest_tools_scrapy",
-                        help="Output filename. Contains 2 different keys: ['stats'] and ['tools_ok']"
+                        help="Output filename. 2 different keys: ['stats'] and ['tools_ok']"
                         )
 
     # Add the argument of the ouput file directory for HTMLs without JavaScript:
@@ -100,7 +106,7 @@ if __name__ == "__main__":
                         help="Name of the output directory for htmls_js"
                         )
 
-    # Add the argument of output's directory name of log:
+    # Add the argument of output's file name of log:
     parser.add_argument(
                         '-log_file_name',
                         type=str,
@@ -109,7 +115,7 @@ if __name__ == "__main__":
                         help="Name of the output log file of the program"
                         )
 
-    # ArgumentParser parses arguments through the parse_args() method. This will inspect the command line, convert each argument to the appropriate type and then invoke the appropriate action. In most cases, this means a simple Namespace object will be built up from attributes parsed out of the command line:
+    # ArgumentParser parses arguments through the parse_args() method.
     args = parser.parse_args()
 
     # Check if the directory exists and if not exists create it:
